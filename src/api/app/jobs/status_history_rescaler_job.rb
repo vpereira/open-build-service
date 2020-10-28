1dbb22007a5ff8a67889ac45fdbf2a56882f96b5
Victor Pereira <vpereira@suse.de>
2020-10-28 18:16:35 +0100

    Refactor StatusHistoryRescalerJob

--- src/api/app/jobs/status_history_rescaler_job.rb
+++ src/api/app/jobs/status_history_rescaler_job.rb
@@ -6,13 +6,13 @@ class StatusHistoryRescalerJob < ApplicationJob
     distinct_status_history_keys.each do |key|
       StatusHistory.transaction do
         # first rescale a month old
-        cleanup(key, offset_from_now(12.hours.ago), 1.month.ago.to_i)
+        cleanup(key, 12.hours.ago, 1.month.ago)
         # now a week old
-        cleanup(key, offset_from_now(6.hours.ago), 7.days.ago.to_i)
+        cleanup(key, 6.hours.ago, 7.days.ago)
         # now rescale yesterday
-        cleanup(key, offset_from_now(1.hour.ago), 24.hours.ago.to_i)
+        cleanup(key, 1.hour.ago, 24.hours.ago)
         # 2h stuff
-        cleanup(key, offset_from_now(4.minutes.ago), 2.hours.ago.to_i)
+        cleanup(key, 4.minutes.ago, 2.hours.ago)
       end
     end
   end
@@ -23,23 +23,19 @@ class StatusHistoryRescalerJob < ApplicationJob
     StatusHistory.distinct.pluck(:key)
   end
 
-  def offset_from_now(date)
-    (Time.now - date).to_i
-  end
-
   def find_items(key, mintime, maxtime)
-    StatusHistory.where('`key` = ? and `time` > ? and `time` < ?', key, mintime, maxtime).order(:time)
+    StatusHistory.where(key: key, time: maxtime..mintime)
   end
 
-  def cleanup(key, mintime, maxtime_offset)
+  def cleanup(key, mintime, maxtime)
     # we try to make sure all keys are in the same time slots,
     # so start with the overall time
-    allitems = find_items(key, mintime, maxtime_offset)
+    allitems = find_items(key, mintime, maxtime)
     return if allitems.empty?
 
     time_average = allitems.average(:time)
     value_average = allitems.average(:value)
-    StatusHistory.delete(allitems.map(&:id))
+    allitems.delete_all
     StatusHistory.create(key: key, time: time_average, value: value_average)
   end
 end
